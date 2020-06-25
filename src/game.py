ec495915678b3e537337ddcc6a3aed39d98b16f2
PiggerZZM <442823793@qq.com>
2020-06-25 17:43:35 +0800

    修复贪吃蛇过早开始运动的BUG，增加日志

--- src/game.py
+++ src/game.py
@@ -1,4 +1,5 @@
 import pygame
+import logging
 
 from src.food import Food
 from src.game_stats import GameStats
@@ -8,32 +9,43 @@ from src.button import Button
 from src.snake import Snake
 from src.wall import Wall
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+
 
 def run_game():
     # 初始化
     pygame.init()
+    logging.info('Pygame初始化成功')
 
     # 实例化游戏设置类
     settings = Settings()
+    logging.info('实例化游戏设置成功')
 
     # 创建屏幕对象
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     pygame.display.set_caption(settings.caption)
+    logging.info('创建屏幕成功')
 
     # 创建开始按钮
     play_button = Button()
+    logging.info('实例化开始按钮成功')
 
     # 创建贪吃蛇对象
     snake = Snake()
+    logging.info('实例化贪吃蛇成功')
 
     # 创建围墙对象
     wall = Wall(settings)
+    logging.info('实例化围墙成功')
 
     # 创建统计信息
     stats = GameStats()
+    logging.info('实例化统计信息成功')
 
     # 创建食物
     food = Food(settings)
+    logging.info('实例化食物成功')
 
     # 控制窗口刷新速度
     fps = 10
@@ -45,7 +57,8 @@ def run_game():
         gf.check_events(play_button, stats, snake)
 
         # 更新蛇身
-        snake.update()
+        if stats.game_active:
+            snake.update()
 
         # 重绘屏幕
         gf.update_screen(screen, settings, play_button, stats, snake, wall, food)
